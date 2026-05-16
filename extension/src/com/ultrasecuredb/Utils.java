2035c707c7c5082fb3a367802e198a63e3cafd22
shreeramcomputers455-oss <shreeramcomputers455@gmail.com>
2026-05-16 05:48:38 +0530

    Create stub classes for extension dependencies

---
+++ extension/src/com/ultrasecuredb/Utils.java
@@ -0,0 +1,19 @@
+package com.ultrasecuredb;
+
+import android.content.Context;
+
+/**
+ * Stub classes for dependencies not implemented in this build
+ */
+
+public class DatabaseManager {
+  public DatabaseManager(Context context) {}
+}
+
+public class MailManager {
+  public MailManager() {}
+}
+
+public class ConfigManager {
+  public ConfigManager(Context context) {}
+}
